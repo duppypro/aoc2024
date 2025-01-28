8e64b9d43488b7973ac345ebdba0d132b1a9f7a1
duppy <duppy@duppy.com>
2025-01-27 19:13:56 -0800

    Day 4 Part One SOLVED if grid_size set manually

--- day4/day4p1.py
+++ day4/day4p1.py
@@ -32,7 +32,7 @@ directions = (
 
 # make grid larger than input by word_length on each side
 # CONSTRAINT: Assumes input is no larger than 140x140
-grid_size = 16
+grid_size = 146
 # Initialize a grid of grid_size tuples of grid_size pad_char
 grid = [pad_char * grid_size] * grid_size
 
@@ -40,17 +40,31 @@ grid = [pad_char * grid_size] * grid_size
 x = word_length - 1
 y = word_length - 1
 for line in sys.stdin:
-    grid[y] = grid[y][:x] + line.strip() + grid[y][grid_size-word_length+1:]
+    line = line.strip()
+    grid[y] = grid[y][:x] + line + grid[y][grid_size - x:]
     y += 1
 
 print(grid)
 y = word_length - 1
 
 def check(grid, start, dir):
-    print(dir, grid[start[1]][start[0]], grid[start[1]+dir[1]][start[0]+dir[0]])
-    return True
+    # print(start, dir, start[1]+dir[1], start[0]+dir[0])
+    # print("\t", grid[start[1]][start[0]])
+    # print("\t\t", grid[start[1]+dir[1]][start[0]+dir[0]])
+    cursor = list(start)
+    for i in range(word_length):
+        if grid[cursor[1]][cursor[0]] != search_word[i]:
+            return 0
+        cursor[0] += dir[0]
+        cursor[1] += dir[1]
+    return 1
 
-for y in range(word_length-1, grid_size - word_length + 1):
-    for x in range(word_length-1, grid_size - word_length + 1):
+total = 0
+for y in range(word_length-1, grid_size - (word_length - 1)):
+    for x in range(word_length-1, grid_size - (word_length - 1)):
+        print(grid[y])
+        print(' ' * x + grid[y][x])
         for dir in range(len(directions)):
-            check(grid, (x, y), directions[dir])
+            total += check(grid, (x, y), directions[dir])
+
+print(total)
